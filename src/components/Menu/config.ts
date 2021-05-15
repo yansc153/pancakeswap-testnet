1ccdcce2da26a319f4c78da6a7fcc327ebb3da5b
tokyo <tokyo@neptunemutual.com>
2021-05-14 23:05:47 -0700

    First commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,31 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://pancake-test.pages.dev',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool',
+      },
+    ],
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: 'https://pancake-test.pages.dev/pools',
+  }
+]
+
+export default config
---
+++ src/components/swap/FormattedPriceImpact.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Percent } from '../../sdk'
+import { ONE_BIPS } from '../../constants'
+import { warningSeverity } from '../../utils/prices'
+import { ErrorText } from './styleds'
+
+/**
+ * Formatted version of price impact text with warning colors
+ */
+export default function FormattedPriceImpact({ priceImpact }: { priceImpact?: Percent }) {
+  return (
+    <ErrorText fontSize="14px" severity={warningSeverity(priceImpact)}>
+      {priceImpact ? (priceImpact.lessThan(ONE_BIPS) ? '<0.01%' : `${priceImpact.toFixed(2)}%`) : '-'}
+    </ErrorText>
+  )
+}
